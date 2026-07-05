20683d0b59b905383d93e831f8a2e4319d3d2aa6
Calbabreaker <calbabreaker@gmail.com>
2026-07-05 19:33:10 +1000

    deploy gh-pages

--- resplay-gui/src/ui_window/stats.rs
+++
@@ -1,7 +0,0 @@
-pub fn show(ui: &mut egui::Ui, state: &mut crate::State) {
-    ui.label(format!(
-        "UI render time: {:.3}ms",
-        state.ui_render_time * 1000.
-    ));
-    ui.label(format!("Frame rate: {:.3}fps", state.emu.frame_rate()));
-}
